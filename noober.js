f777eb0c4608547bb8ef7e7890d94e9fb380d2bf
Jason Davis <jason.davis@kellogg.northwestern.edu>
2021-04-19 15:47:23 -0500

    Full code for HW3

--- noober.js
+++ noober.js
@@ -13,5 +13,34 @@ document.addEventListener('DOMContentLoaded', async function() {
   // 🔥 YOUR CODE GOES HERE 🔥
   // Write the recipe (algorithm) in the comments. Then, write the code.
   
+  // Parse object and create well-named variables
+
+  // Set Passenger name to First Name and Last Name, separated with a space
+  let passengerName = `${ride.passengerDetails.first} ${ride.passengerDetails.last}`
+  // Set Passenger Phone Number to the phone number and store as a string
+  let passengerPhoneNumber = `${ride.passengerDetails.phoneNumber}`
+  // Set Pickup Location to the Address, City, State and Zip
+  let pickupLocation = `${ride.pickupLocation.address}, ${ride.pickupLocation.city}, ${ride.pickupLocation.state} ${ride.pickupLocation.zip}`
+  // Set Dropoff Location to the Address, City, State and Zip
+  let dropoffLocation = `${ride.dropoffLocation.address}, ${ride.dropoffLocation.city}, ${ride.dropoffLocation.state} ${ride.dropoffLocation.zip}`
+  // Store number of passengers in memory
+  let numberOfPassengers = ride.numberOfPassengers
+
+  // Set level of service with conditional statements
+  // If Purple is requested, service is always purplee
+  // Otherwise it is a Noober X, unless there are more than 3 people in car (then Noober XL)
+
+  let serviceRequested
+  if (ride.purpleRequested==true) {
+    serviceRequested=`Noober Purple`
+  } else if (ride.numberOfPassengers > 3 && ride.purpleRequested==false){
+    serviceRequested=`Noober XL`
+  } else {
+    serviceRequested=`Noober X`
+  }
+
+  // Print ride in Console in human-readable format
+  console.log(`${serviceRequested} Passenger: ${passengerName} - ${passengerPhoneNumber}. Pickup at ${pickupLocation}. Drop-off at ${dropoffLocation}.`)
+
   // 🔥 YOUR CODE ENDS HERE 🔥
 })
